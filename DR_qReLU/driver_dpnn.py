c398e9b64e16f397f4fc95331369ba3b1287dd4a
qiwang7777 <135851405+qiwang7777@users.noreply.github.com>
2026-05-18 12:19:40 +0200

    Update driver_dpnn.py

--- DR_qReLU/driver_dpnn.py
+++ DR_qReLU/driver_dpnn.py
@@ -1,7 +1,7 @@
 from .training_helper import plot_tr_history, plot_solution_and_error,train_poisson_with_TR, vector_from_model, make_training_points_grid, compute_g_from_u_star, kappa_xy
 import torch
 from .qReLU_NN import PoissonNet
-from .Poisson_obj import PoissonCompositeObjective
+from .Poisson_obj import PoissonCompositeObjective, u_star
 from semismooth_TR.derivative_check import grad_check, gn_quadratic_form_check
 
 if __name__ == "__main__":
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     xy = make_training_points_grid(ngrid, device = device)
     g = compute_g_from_u_star(xy)
     var = {"useEuclidean": False, "beta": beta}
-    obj_smooth = PoissonCompositeObjective(model = model, xy= xy, g = g, kappa_fn = kappa_xy, weight = None, device = device, mu_I = 0.0)
+    obj_smooth = PoissonCompositeObjective(model = model, xy= xy, g = g, kappa_fn = kappa_xy, weight = None, device = device, mu_I = 0.0, x_true=True, u_true_fn=u_star)
     obj_smooth.set_hess_mode("full")
     x0 = vector_from_model(model)
     #Derivative check and Hessian check
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     torch.autograd.set_detect_anomaly(True)
     
 
-    model, x_opt, cnt = train_poisson_with_TR(
+    model, x_opt, cnt,_ = train_poisson_with_TR(
         width=width,
         depth=depth,
         ngrid=ngrid,
