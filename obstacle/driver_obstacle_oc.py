b8cdc645c0ef8f3e7a6ec39e09bac4b359e8b43e
qiwang7777 <135851405+qiwang7777@users.noreply.github.com>
2026-07-01 15:48:27 +0200

    Update driver_obstacle_oc.py

--- obstacle/driver_obstacle_oc.py
+++ obstacle/driver_obstacle_oc.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         beta=1e-6,
         gamma=10,
         delta0=1.0,
-        maxit=1000,
+        maxit=400,
         device=device,
         mu_smooth=1e-6,
         deltamin=1e-5,
