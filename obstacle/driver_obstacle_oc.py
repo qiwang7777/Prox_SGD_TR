0de31b39203d56cd715acbe3ca034dfcbb714cdd
qiwang7777 <135851405+qiwang7777@users.noreply.github.com>
2026-07-03 13:16:15 +0200

    Update driver_obstacle_oc.py

--- obstacle/driver_obstacle_oc.py
+++ obstacle/driver_obstacle_oc.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     obj, u_opt, cnt, best_u = train_obstacle_oc_with_TR(
         n=64,
-        alpha=1e-2,
+        alpha=1e-4,
         beta=1e-7,
         gamma=1e5,
         delta0=1.0,
