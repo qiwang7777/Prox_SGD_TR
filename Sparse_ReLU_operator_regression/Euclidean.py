9777529caab13c465f203486b01976a9afd5e9c3
qiwang7777 <135851405+qiwang7777@users.noreply.github.com>
2026-07-15 13:55:16 +0200

    Add files via upload

---
+++ Sparse_ReLU_operator_regression/Euclidean.py
@@ -0,0 +1,35 @@
+import torch
+
+
+class EuclideanPrimal:
+    def __init__(self, var=None):
+        self.var = var
+
+    @torch.no_grad()
+    def dot(self, x, y):
+        return float(torch.sum(x.data * y.data).item())
+
+    @torch.no_grad()
+    def norm(self, x):
+        return float(torch.linalg.vector_norm(x.data).item())
+
+
+class EuclideanDual:
+    def __init__(self, var=None):
+        self.var = var
+
+    @torch.no_grad()
+    def apply(self, x, y):
+        return float(torch.sum(x.data * y.data).item())
+
+    @torch.no_grad()
+    def dual(self, x):
+        return x
+
+
+class L2Primal(EuclideanPrimal):
+    pass
+
+
+class L2Dual(EuclideanDual):
+    pass
